6fa096596199b5ef811781b4f782b1b1fc59008f
LYJ <996979356@qq.com>
2018-01-15 17:31:31 +0800

    fix route address

--- my_django_test_one/polls/urls.py
+++ my_django_test_one/polls/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
 
     path('', views.index, name='index'),
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('polls/<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('polls/<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
 
 ]
